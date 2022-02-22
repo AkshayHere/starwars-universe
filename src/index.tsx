dd599f65777d5ff1c2ec58ab97cd2dd4344de551
Akshay Mohan <akshay.mohan@sensiaglobal.com>
2022-02-22 16:20:02 +0800

    Changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import 'antd/dist/antd.css';
-import Home from './Home';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware, Store } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import "antd/dist/antd.css";
+import Home from "./Home";
+
+import reducer from "./store/reducer";
+
+const store: Store<ArticleState, ArticleAction> & {
+  dispatch: DispatchType;
+} = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <Provider store={store}>
+      <Home />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
